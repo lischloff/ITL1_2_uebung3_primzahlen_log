a6c41daa95c8a921c9d8bc1b8de4247b302fb411
lischloff <lisa.schloffer@gmail.com>
2025-11-18 14:56:42 +0100

    Input Feld

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aufgabe3_Schloffer</title>
+</head>
+
+<body>
+    <h1>Primzahlen/Log</h1>
+
+  
+      <!-- Formular für die Eingabe der Obergrenze -->
+  <form action="" method="post">
+      <label for="zahl1">Geben Sie eine Zahl ein:</label><br>
+      <input type="text" id="zahl1" name="zahl1" value="<?=$zahl1?>"><br><br>
+      <input type="submit" name="submit" value="Primzahlen anzeigen"><br><br>
+  </form>
+
+ 
+</body>
+</html>
